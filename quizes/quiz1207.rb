29a7d5bd33782c58a4a82053050bdcb3564d705b
Jonathan Ku <jonathanku88@gmail.com>
2012-12-07 09:49:39 -0500

    quizes

---
+++ quizes/quiz1207.rb
@@ -0,0 +1,14 @@
+1.  HTML is markup language for structure on a site
+2. SASS allows you to nest CSS and divide up the files better
+3. boolean, integer, string...
+4. create an object in javascript?
+5. JSON allows you to parse data into a hash
+6. dom ready in javascript means the DOM is ready to receive requests from your code
+7. Global you have to declare it outside the function, local is inside
+8. every class is a subclass of the object that is created
+9. Inheritance is carrying over attributes from one class to the other
+10. hash has key values and is faster to access than a hash which has an index value
+11. hash is able to access something from it faster, array is better for simpler data
+12. stack, queue, tree
+13. CHi has about 1,500,000, use top 10%, 15000
+14. so the cover cant fall in easier, and you can roll them around
